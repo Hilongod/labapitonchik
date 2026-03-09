b5fb91c2aee4babba5b427288eef99329272a3c0
Hilongod <17_seva@mail.ru>
2026-03-10 00:59:29 +0500

    Add files via upload

--- lab2/HILONlab2(1).py
+++ lab2/HILONlab2(1).py
@@ -1,11 +1,12 @@
 from itertools import product
 
-alf = ['A', 'B', 'C', 'D', 'E']
+def f():
+    alf = ['A', 'B', 'C', 'D', 'E']
+    s = [
+        ''.join(i)
+        for i in product(alf, repeat = 5)
+        if not (i[0] == 'E' and i[4] == 'A') #обрабатываем 1 буква не Е и ластовая не А
+    ]
+    return len(s)
 
-valid_words = [
-    ''.join(i)
-    for i in product(alf, repeat=5)
-    if not (i[0] == 'E' and i[4] == 'A') #обрабатываем 1 буква не Е и ластовая не А
-]
-
-print(len(valid_words))  
+print(f())
--- lab2/HILONlab2(2).py
+++ lab2/HILONlab2(2).py
@@ -1,4 +1,4 @@
-a = 4**511 + 2**511 - 511
-i = bin(a)
-res = i.count('1')
-print(res)
+def f():
+    a = 4**511 + 2**511 - 511
+    return bin(a).count('1')
+print(f())
